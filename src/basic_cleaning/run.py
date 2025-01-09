08fba81962164a3b6ddd30567e85e381510da4b8
Erick <ga74kob@mytum.de>
2025-01-09 15:01:53 +0100

    added step for cleaning: position in correct geolocation

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -34,6 +34,10 @@ def go(args):
     logger.info(f"Cleaning sample df")
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
+    # cleaning after "successful failure"
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
     # saving to a file
